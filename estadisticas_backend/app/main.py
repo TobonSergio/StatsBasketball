0a0892b561e5f7fbecc74df51224de90c3ba0f6a
SergioTobon <sestebantmontoya@gmail.com>
2026-04-24 17:09:48 -0500

    Linea de errores de pull resultos

--- estadisticas_backend/app/main.py
+++ estadisticas_backend/app/main.py
@@ -3,12 +3,11 @@ from app.api.v1.routers import teams
 from app.api.v1.routers import players
 from app.api.v1.routers import games
 from app.api.v1.routers import games_players
-<<<<<<< HEAD
+
 from fastapi.middleware.cors import CORSMiddleware
-=======
+
 from app.api.v1.routers import events
 from app.api.v1.routers import stats
->>>>>>> aedda2942c2a4e6c9749cdd4a281ac320960b455
 
 app = FastAPI(title="Basketball Stats API")
 
@@ -16,7 +15,6 @@ app.include_router(teams.router)
 app.include_router(players.router)
 app.include_router(games.router)
 app.include_router(games_players.router)
-<<<<<<< HEAD
 
 app.add_middleware(
     CORSMiddleware,
@@ -25,7 +23,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-=======
+
 app.include_router(events.router)
 app.include_router(stats.router)
->>>>>>> aedda2942c2a4e6c9749cdd4a281ac320960b455
+
